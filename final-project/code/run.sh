c2cbb6c8db0e0110d0fd9d205c02b469bb0d7731
KurtSoncco <150975966+KurtSoncco@users.noreply.github.com>
2024-12-13 21:27:05 -0800

    Updated  run.sh

--- final-project/code/run.sh
+++ final-project/code/run.sh
@@ -6,7 +6,10 @@ conda activate 215a
 # Install libraries required in environment from yaml file
 conda env update --file environment.yaml
 
-# Run the script
+# Run the script for data cleaning
 python clean.py
+
+# Run the analysis script
+python analysis.py
 
 
