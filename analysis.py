42fb9c5fb2999ba2f83d9e64bc7df7da98ae2e81
KurtSoncco <150975966+KurtSoncco@users.noreply.github.com>
2024-12-13 22:51:10 -0800

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -7,10 +7,15 @@ def run_file(file_name):
         subprocess.run(['jupyter', 'nbconvert', '--to', 'notebook', '--execute', file_name])
 
 files_to_run = [
+    'EDA.py',
     'logistic_regression.py',
     'neural_network.py',
     'random_forest_model.py',
-    'jacob_ml.ipynb'
+    'jacob_ml.ipynb',
+    'tbfn.py'
+    'nn.py',
+    'neural_network.py',
+    'clean_RF_features.py'
 ]
 
 for file in files_to_run:
